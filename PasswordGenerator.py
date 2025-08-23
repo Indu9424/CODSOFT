d7e3e91f92cbd98a9b26ec069dbe3d31cb66be2a
Indu <addulaindu@gmailcom>
2025-08-23 18:10:36 +0530

    PAssword Generator Project

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -3,7 +3,7 @@ import string
 
 length=int(input("Enter the length of the password: ")) 
 def password_generator(length):
-    characters=string.asscii_letters+string.digits+string.punctuation 
+    characters = string.ascii_letters + string.digits + string.punctuation
     password = ''.join([random.choice(characters) for i in range(length)])
     return password
 

a4f029eb8e04d3ed5d6a677ea770983530ff8eee
Indu <addulaindu@gmailcom>
2025-08-23 18:02:55 +0530

    Password Generator Project

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -5,7 +5,7 @@ length=int(input("Enter the length of the password: "))
 def password_generator(length):
     characters=string.asscii_letters+string.digits+string.punctuation 
     password = ''.join([random.choice(characters) for i in range(length)])
-        return password
+    return password
 
-password=password_generator(length)
-print("Generated Password: ",password)
+result=password_generator(length)
+print("Generated Password: ",result)
